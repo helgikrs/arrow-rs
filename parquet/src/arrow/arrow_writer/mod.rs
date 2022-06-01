98bb9981e343bf78655da22996692fb851363a67
Helgi Sigurbjarnarson <helgi@lacework.net>
2022-06-01 11:30:46 -0700

    Min row group size

--- parquet/src/arrow/arrow_writer/mod.rs
+++ parquet/src/arrow/arrow_writer/mod.rs
@@ -120,6 +120,7 @@ pub struct ArrowWriter<W: Write> {
 
     /// The length of arrays to write to each row group
     max_row_group_size: usize,
+    min_row_group_size: usize,
 }
 
 impl<W: Write + Send> std::fmt::Debug for ArrowWriter<W> {
@@ -168,6 +169,7 @@ impl<W: Write + Send> ArrowWriter<W> {
         }
 
         let max_row_group_size = props.max_row_group_size();
+        let min_row_group_size = props.min_row_group_size();
 
         let file_writer =
             SerializedFileWriter::new(writer, schema.root_schema_ptr(), Arc::new(props))?;
@@ -177,6 +179,7 @@ impl<W: Write + Send> ArrowWriter<W> {
             in_progress: None,
             arrow_schema,
             max_row_group_size,
+            min_row_group_size,
         })
     }
 
@@ -212,7 +215,7 @@ impl<W: Write + Send> ArrowWriter<W> {
 
     /// Encodes the provided [`RecordBatch`]
     ///
-    /// If this would cause the current row group to exceed [`WriterProperties::max_row_group_size`]
+    /// If this would cause the current row group to exceed [`WriterProperties::min_row_group_size`]
     /// rows, the contents of `batch` will be written to one or more row groups such that all but
     /// the final row group in the file contain [`WriterProperties::max_row_group_size`] rows.
     ///
@@ -242,7 +245,7 @@ impl<W: Write + Send> ArrowWriter<W> {
 
         in_progress.write(batch)?;
 
-        if in_progress.buffered_rows >= self.max_row_group_size {
+        if in_progress.buffered_rows >= self.min_row_group_size {
             self.flush()?
         }
         Ok(())
